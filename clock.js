f7dff4e4dad7282df93740ee49e669202dc28d56
tomo <tomo.mouse.9086@gmail.com>
2017-04-21 12:07:28 +0430

    Tehran bugfix

--- clock.js
+++ clock.js
@@ -1,4 +1,8 @@
-document.getElementById("timezone").innerHTML = new Date().toString().match(/\(([A-Za-z\s].*)\)/)[1];
+try {
+	document.getElementById("timezone").innerHTML = new Date().toString().match(/\(([A-Za-z\s].*)\)/)[1];
+} catch(err) {
+	document.getElementById("timezone").innerHTML = "- - -";
+}
 setInterval(function() {
 	const date = new Date();
 	const hour = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
